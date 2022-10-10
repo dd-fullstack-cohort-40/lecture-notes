6b0b1a4c489f0435535d066add1e379551a13564
martybonacci <martybonacci@gmail.com>
2022-10-10 09:52:51 -0600

    function return

---
+++ code-alongs/more-functions/function-return.js
@@ -0,0 +1,11 @@
+function pBAndJ () {
+  return 'peanut butter and jelly sandwich'
+}
+let sandwich = pBAndJ()
+console.log(sandwich)
+
+function makeSandwich (typeOfSandwich) {
+  return typeOfSandwich + ' sandwich'
+}
+
+console.log(makeSandwich('ham and swiss'))
