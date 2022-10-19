3f3214010b418791d1dbf2d17430c5fb64478498
martybonacci <martybonacci@gmail.com>
2022-10-19 10:51:08 -0600

    js events fun

--- week-03/javascript/example-code/script.js
+++ week-03/javascript/example-code/script.js
@@ -1,3 +1,19 @@
 function targetButton() {
   console.log('someone clicked my button')
+  let target = document.getElementById('target')
+  console.log(target)
+  if (target.innerText === 'click me') {
+    target.style.borderColor = 'red'
+    target.innerText = 'clicked'
+    let h1Tags = document.getElementsByTagName('h1')
+    console.log(h1Tags)
+    h1Tags[0].innerText = 'Hi Nycole'
+  } else {
+    target.style.borderColor = ''
+    target.innerText = 'click me'
+    let h1Tags = document.getElementsByTagName('h1')
+    console.log(h1Tags)
+    h1Tags[0].innerText = 'Javascript events demo'
+  }
+
 }
